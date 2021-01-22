0fb301c47bb1cafa3c8661e890555cbfd5f44f79
LI XIAN ZHANG <s1009106@gmail.com>
2021-01-22 20:59:37 +0800

    correct version

--- number.py
+++ number.py
@@ -1,15 +1,14 @@
 #猜數字遊戲 練習
 import random
 number = random.randint(1, 100)
-ans = input('猜出正確數字，請輸入1到100的數字:')
-ans = int(ans)
-i = 100
-while i > 0:
-	i = i - 1
+
+while True:
+	ans = input('猜數字，請輸入1到100的數字:')
+	ans = int(ans)
 	if number == ans:
 		print('恭喜你答對了')
 		break
-	elif number >= ans:
-		print('再小一點')
-	else:
+	elif number > ans:
 		print('再大一點')
+	elif number < ans:
+		print('再小一點')
