83537b8ad33c73192fcbc1c459f30fcb03e9ddd7
LI XIAN ZHANG <s1009106@gmail.com>
2021-01-22 21:20:19 +0800

    final version

--- number.py
+++ number.py
@@ -1,10 +1,16 @@
 #猜數字遊戲 練習
 import random
-number = random.randint(1, 100)
+
+start = input('請輸入隨機數字開始值:')
+end = input('請輸入隨機數字結束值:')
+start = int(start)
+end = int(end)
+number = random.randint(start, end)
+
 count = 0
 while True:
 	count += 1 #就是count = count + 1的快寫法
-	ans = input('猜數字，請輸入1到100的數字:')
+	ans = input('請猜數字:')
 	ans = int(ans)
 	if number == ans:
 		print('恭喜你答對了')
